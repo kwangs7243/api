3ae8a3563feeaf72b428bf0590eb061088c70eec
kwang <kwangs7243@gmil.com>
2026-04-12 19:23:19 +0900

    accountbook(model)2

--- models/accountbook_model.py
+++ models/accountbook_model.py
@@ -24,9 +24,9 @@ class AccountBookModel:
         cursor.execute(sql(user_id, category, amount, content, False))
         conn.commit()
         conn.close()
-    # 가계부 목록 가져오기
-    def get_user_AB(self, user_id, keyword="", category=None, sort_by="created_at", order="desc"):
-        where_clauses = ["user_id = %s", "deleted = %s"]
+    # 가계부 내역 가져오기
+    def get_user_transactions(self, user_id, keyword="", category=None, sort_by="created_at", order="desc"):
+        where_clauses = []
         params = [user_id, False]
 
         keyword = keyword.strip()
@@ -38,27 +38,39 @@ class AccountBookModel:
             where_clauses.append("category = %s")
             params.append(category)
         
-        if sort_by not in ["created_at", "content", "amount"]:
-            sort_by = "created_dy"
+        if sort_by not in ["created_at", "content", "amount", "balance"]:
+            sort_by = "created_at"
         
         order = order.lower()
         if order not in ["asc", "desc"]:
             order = "desc"
+
+        outer_where = ""
+        if where_clauses:
+            outer_where = "WHERE" + " AND ".join(where_clauses)
         
         conn = db_connect()
         cursor = conn.cursor()
         sql = f"""
-                SELECT *
+                SELECT id, user_id, category, amount, content, balance, created_at
+                    FROM (
+                        SELECT id, user_id, category, amount, content, deleted, created_at,
+                        sum(CASE WHEN category = 'income' THEN amount
+                                WHEN category = 'expense' THEN -amount
+                                ELSE 0
+                            END
+                            ) OVER (ORDER BY created_at ASC, id ASC) AS balance
                     FROM accountbook
-                    WHERE {" AND ".join(where_clauses)}
-                    ORDER BY {sort_by}, id {order}
+                    WHERE user_id = %s AND deleted = %s
+                    ) AS sub
+                    {outer_where}
+                    ORDER BY {sort_by} {order} , id {order}
                 """
         cursor.execute(sql, params)
-        accountbook = cursor.fetchall()
+        transactions = cursor.fetchall()
         conn.close()
-        return accountbook
-    
-    
+        return transactions
+
 
 
 
